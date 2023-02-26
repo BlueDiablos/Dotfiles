2a4daa380a6c0c7b2ebac62749bb5a7c1472c72e
BlueDiablos <jmichaelblackledge@gmail.com>
2023-02-25 21:01:33 -0500

    initial commit

---
+++ AppData/Local/nvim/lua/bluediablos/init.lua
@@ -0,0 +1,2 @@
+require("bluediablos.remap")
+require("bluediablos.set")
---
+++ AppData/Local/nvim/lua/bluediablos/remap.lua
@@ -0,0 +1,4 @@
+vim.g.mapleader = " "
+vim.keymap.set("n","<leader>pv", vim.cmd.NERDTreeToggle)
+
+vim.keymap.set("n","<leader>fr", vim.lsp.buf.format)
