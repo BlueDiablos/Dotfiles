7f568e042a3b17ecaae42f5808ae6447e567ae7d
Joseph Blackledge <jblackledge@protonmail.com>
2024-02-25 17:50:23 -0500

    Added setup script and moved alacritty into nested directory

---
+++ setup.ps1
@@ -0,0 +1,11 @@
+##creates symlinks between this repos config directories and the ones specified by the user
+
+$NeovimFolder = Read-Host "Please enter the neovim symlink location..."
+$AlacrittyFolder = Read-Host "Please enter the alacritty symlink location..."
+
+
+Write-Output 'Creating sym links for neovim...'
+New-Item -Path $NeovimFolder -ItemType SymbolicLink -Value './nvim/'
+
+Write-Output 'Creating sym links for allacritty...'
+New-Item -Path $AlacrittyFolder -ItemType SymbolicLink -Value './alacritty/'
